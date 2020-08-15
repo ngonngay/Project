1f61a002b47e441c285ebe057bc3ae772f97b48e
ngonngay <thangnguyenquyetvlth@gamil.com>
2020-08-15 15:26:59 +0700

    change at test package

--- testjavaapp/src/test/java/vn/edu/vtc/dal/AccountDALTest.java
+++ testjavaapp/src/test/java/vn/edu/vtc/dal/AccountDALTest.java
@@ -16,59 +16,7 @@ Order{productList=[Product{product_id=1001, name='phobo', price=10000.0, discoun
 */
 public class AccountDALTest {
 
-    @Test
-    public void validateUsername(){
-        //correct username form
-        Assert.assertTrue(PasswordService.validateUsername("Nguyenquyetthang"));
-    }
-    @Test
-    public void validateUsername2(){
-         
-        //have special character
-        Assert.assertFalse(PasswordService.validateUsername("Nguyenquyetthang123%"));
-    }
-    @Test
-    public void validateUsername3(){
-         
-        //less than 8 character
-        Assert.assertFalse(PasswordService.validateUsername("Nguy"));
-    }
-    @Test
-    public void validateUsername4(){
-         
-        //more than 20 character
-        Assert.assertFalse(PasswordService.validateUsername("Nguyenquyetthang123asdvghasdvghasvghasvdhvas"));
-    }
-    @Test
-    public void validatePassword(){
-         
-        //correct password
-        Assert.assertTrue(PasswordService.validatePassword("Thangnguyenquyet123"));
-    }
-    @Test
-    public void validatePassword2(){
-         
-        //It wasn't contains at least one digit.
-        Assert.assertFalse(PasswordService.validatePassword("Thangnguyenquyet"));
-    }
-    @Test
-    public void validatePassword3(){
-         
-        //It wasn't contains at least upper case alphabet.
-        Assert.assertFalse(PasswordService.validatePassword("thangnguyenquyet123"));
-    }
-    @Test
-    public void validatePassword4(){
-         
-        //It wasn't contains at least lower case alphabet.
-        Assert.assertFalse(PasswordService.validatePassword("THANGNGUYENQUYET"));
-    }
-    @Test
-    public void testMD5(){
-        String result=StaticFuncitionService.getMd5("Thangnguyenquyet123");
-        String expected="16c0ce36e334e22fda8caca1b10c2f9c";
-        Assert.assertTrue(result.equals(expected));
-    }
+    
     @Test
     public void getAccountTest1(){
         AccountDAL accountDAL=new AccountDAL();
